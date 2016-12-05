28705caa0a97724970527f23f0dc763cd998d825
soranoba <soranoba@gmail.com>
2016-12-05 23:09:03 +0900

    support minous number, float, boolean

---
+++ MantleXMLExtensionTests/Models/MXETTypeModel.h
@@ -0,0 +1,20 @@
+//
+//  MXETTypeModel.h
+//  MantleXMLExtension
+//
+//  Created by Hinagiku Soranoba on 2016/12/05.
+//  Copyright © 2016年 Hinagiku Soranoba. All rights reserved.
+//
+
+#import "MXEXmlAdapter.h"
+#import <Foundation/Foundation.h>
+#import <Mantle/MTLModel.h>
+
+@interface MXETTypeModel : MTLModel <MXEXmlSerializing>
+
+@property (nonatomic, assign) NSInteger intNum;
+@property (nonatomic, assign) double doubleNum;
+@property (nonatomic, assign) float floatNum;
+@property (nonatomic, assign) BOOL boolNum;
+
+@end
